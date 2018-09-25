d88afb21e1bf01e7e6fbf259db40b1e4d37b30e8
astu <astu@kapsi.fi>
2018-09-25 21:32:41 +0300

    catch exeptions and generate errors

--- controllers/BlogRouter.js
+++ controllers/BlogRouter.js
@@ -9,15 +9,25 @@ BlogRouter.get('/', (request, response) => {
     .then(blogs => {
       response.json(blogs.map(a => a.format))
     })
+    .catch( (e) => {
+      response.status(500).json( {'error':e} );
+    })
 })
 
 BlogRouter.post('/', (request, response) => {
   console.log('BlogRouter.post');
+  if ( !request.body.title )
+    response.status(400).json({"error":"missing title"});
+  if ( !request.body.url )
+    response.status(400).json({"error":"missing url"});
   const blog = new Blog(request.body)
   blog
     .save()
     .then(result => {
       response.status(201).json(result.format)
+    })
+    .catch( (e) => {
+      response.status(500).json( {'error':e} );
     })
 })
 
