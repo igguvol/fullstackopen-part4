d58eeaa3c39e1defee38e847a830a08aa5666092
astu <astu@kapsi.fi>
2018-09-25 23:04:53 +0300

    blogsInDb helper

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const Blog = require('../models/Blog.js')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -6,8 +8,17 @@ const totalLikes = (blogs) => {
   return blogs.map( a => a.likes ).reduce( (a,b) => a+b );
 }
 
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+    .catch( (e) => {
+      console.log('blogsInDb error: ', e);
+      return e;
+    })
+  //console.log('------------------------------ ',blogs);
+  return blogs.map( (a) => a.format )
+}
 
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, blogsInDb
 
 }
