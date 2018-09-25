b98c9db80bfe83bb14b32c02df8decfb448c2382
astu <astu@kapsi.fi>
2018-09-25 21:32:57 +0300

    more tests

--- tests/get_blogs.test.js
+++ tests/get_blogs.test.js
@@ -3,9 +3,8 @@ const { app, server } = require('../index')
 const api = supertest(app)
 const listHelper = require('../utils/list_helper')
 
-test('dummy is called', () => {
+test('dummy test', () => {
   const blogs = []
-
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
@@ -16,6 +15,31 @@ test('test get blogs', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
+
+test('total likes', async () => {
+  await api.get('/api/blogs')
+  .expect(200)
+  .expect('Content-Type', /application\/json/)
+})
+
+test('POST to blogs', async () => {
+  const testEntry = {'title':'test','author':'test','url':'none'};
+  await api.post('/api/blogs')
+  .send(testEntry)
+  .expect(201)
+  .expect('Content-Type', /application\/json/)
+})
+
+test('POST to blogs with empty "likes"', async () => {
+  const testEntry = {'title':'test','author':'test','url':'none'};
+  const response = await api.post('/api/blogs')
+    .send(testEntry)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  expect(response.test).toBe(0);
+})
+
+
 
 afterAll(() => {
   server.close()
